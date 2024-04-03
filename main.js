43f420756361fbbbb9e1fe0d536c771bec4ab112
dr.minahil16@gmail.com <dr.minahil16@gmail.com>
2024-04-03 15:30:14 +0500

    Adding a To-do-list

---
+++ main.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inqurier from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inqurier.prompt([{
+            name: 'todos',
+            type: 'input',
+            message: "what do you want to add in yout todos?"
+        },
+        {
+            name: 'addMore',
+            type: 'confirm',
+            message: "do you want to add more?",
+            default: "false"
+        }
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(addTask.todos);
+}
+;
